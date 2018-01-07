b8a909d1589b4d5e9aff76e807454eb8df9ce6a1
vidhukar <sanjay@sanjays-imac.lan>
2018-01-07 00:35:35 -0500

    first commit

---
+++ src/main/java/com/greendreams/example/SpringBatchBasicApplication.java
@@ -0,0 +1,12 @@
+package com.greendreams.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchBasicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/greendreams/example/SpringBatchBasicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greendreams.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBatchBasicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
